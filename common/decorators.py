8a81980fc4076d6f51b1652f6148f9b3d318cfa3
Eryk <erykkurkowski.praca@gmail.com>
2024-03-09 18:25:14 +0100

    Dodanie stronicowania AJAX do widoków listy

--- common/decorators.py
+++ common/decorators.py
@@ -1,11 +1,10 @@
 from django.http import HttpResponseBadRequest
 
-
 def ajax_required(f):
     def wrap(request, *args, **kwargs):
-        if not request.is_ajax():
+        if request.headers.get('X-Requested-With') != 'XMLHttpRequest':
             return HttpResponseBadRequest()
         return f(request, *args, **kwargs)
-    wrap.__doc__=f.__doc__
-    wrap.__name__=f.__name__
+    wrap.__doc__ = f.__doc__
+    wrap.__name__ = f.__name__
     return wrap
--- images/views.py
+++ images/views.py
@@ -4,10 +4,13 @@ from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
-
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, \
+                                  PageNotAnInteger
 from common.decorators import ajax_required
 from .forms import ImageCreateForm
 from .models import Image
+
 
 # Create your views here.
 
@@ -61,3 +64,29 @@ def image_like(request):
         except:
             pass
     return JsonResponse({'status':'ok'})
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # Jeżeli zmienna page nie jest liczbą całkowitą, wówczas pobierana jest pierwsza strona wyników
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            # Jeżeli żądanie w technologii AJAX i zmienna page ma wartość spoza zakresu,
+            # wówczas zwracana jest pusta strona.
+            return HttpResponse('')
+        # Jeżeli zmienna page ma wartość większą niż numer ostatniej strony wyników, wtedy pobierana
+        # jest ostatnia strona wyników
+        images = paginator.page(paginator.num_pages)
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        return render(request,
+                      'images/image/list_ajax.html',
+                      {'section': 'images', 'images': images})
+    return render(request,
+                  'images/image/list.html',
+                  {'section': 'images', 'images': images})
