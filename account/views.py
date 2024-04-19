72b4c54e5fd30d8c9afdb9b59a2da600048b8a3b
Eryk <erykkurkowski.praca@gmail.com>
2024-04-19 20:00:32 +0200

    Wywietlanie strumienia aktywnosci, optymalizacja kolekcji QuerySet dotyczacej powiazanych obiektów, tworzenie szablonow dla akcji

--- account/views.py
+++ account/views.py
@@ -37,9 +37,21 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
+    # Domyślnie wyświetlane są wszystkie akcje
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id',
+                                                       flat=True)
+    if following_ids:
+        # Jeżeli użytkownik obserwuje innych, będzie otrzymywał jedynie informacje
+        # o podejmowanych przez nich akcjach.
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile')\
+                     .prefetch_related('target')[:10]
+
     return render(request,
                   'account/dashboard.html',
-                  {'section': 'dashboard'})
+                  {'section': 'dashboard',
+                   'actions': actions})
 
 
 def register(request):
